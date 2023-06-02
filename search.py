267fcaffec06a199c6609e482adcf44f6708ece9
ETRO_secondleader <118088966+ZYpS-leader@users.noreply.github.com>
2023-06-02 11:05:02 +0800

    search.py

--- search.py
+++ search.py
@@ -1,3 +1,16 @@
 import socket
-import request
-import urllib.request
+import requests
+import urllib.requests
+import jieba
+from lxml import etree
+def search(name:str) :->str
+  nali=jieba.cut(name)
+  for i in nali:
+    url = "https://baike.baidu.com/item/"+i
+    
+    r = requests.get(url)#获得服务器响应
+    html_code = r.content.decode(r.encoding)#解码
+    
+    treeObj = etree.HTML(html_code)
+    target = treeObj.xpath("//div[@class='para MARK_MODULE']/div[@data_pid='1']/p/span[@class='short']/text()")
+    return target
